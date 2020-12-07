be7c7a43af9c67cf7f73e9632f18caf9744107d8
Frank <31396084+fhidalgor@users.noreply.github.com>
2020-12-07 14:25:16 -0800

    fixed bug

--- mv_dashboard.py
+++ mv_dashboard.py
@@ -13,10 +13,16 @@ import dash_bio as dashbio
 import dash_html_components as html
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
-from dash_bio_utils import pdb_parser as parser
+#from dash_bio_utils import pdb_parser as parser
 from dash.dependencies import Input, Output
 import json
 import tempfile
+import re
+import os
+from shutil import copy2
+
+import parmed as pmd
+
 
 import copy
 import pandas as pd
@@ -44,7 +50,8 @@ except ModuleNotFoundError:  # This step is only for when I run the notebooks lo
 def _parse_pdb(pdb):
     '''return the pdb in jason format'''
     # Parse pdb file
-    modata = parser.create_data(pdb)
+    modata = parser(pdb)
+    #modata = parser.create_data(pdb)
 
     # Put in jason format
     fmodel = _files_data_style(modata)
@@ -139,6 +146,117 @@ def _files_data_style(content):
     dataFile = fdat.name
     fdat.close()
     return dataFile
+
+
+# ## PDB parser
+
+# In[ ]:
+
+
+def parser(pdb_path):
+    """
+    Parse the protein data bank (PDB) file to generate
+    input modelData
+    @param pdb_path
+    Name of the biomolecular structure file in PDB format
+    """
+
+    # Create local copy of temp file
+    copy2(pdb_path, './tmp.pdb')
+
+    # Use parmed to read the bond information from temp file
+    top = pmd.load_file('tmp.pdb')
+
+    # Remove the created temp file
+    #os.remove('tmp.pdb') #was giving error when uploading to huroku
+
+    # Read PDB file to create atom/bond information
+    with open(pdb_path, 'r') as infile:
+        # store only non-empty lines
+        lines = [l.strip() for l in infile if l.strip()]
+
+    # Initialize all variables
+    var_nchains = []
+    serial = []
+    atm_name = []
+    res_name = []
+    chain = []
+    res_id = []
+    positions = []
+    occupancy = []
+    temp_factor = []
+    atom_type = []
+    ct = 0
+
+    datb = {
+        'atoms': [],
+        'bonds': []
+    }
+
+    # Variables that store the character positions of different
+    # parameters from the molecule PDB file
+    serialpos = [6, 11]
+    atm_namepos = [12, 16]
+    r_namepos = [17, 20]
+    chainpos = [21, 22]
+    r_idpos = [22, 26]
+    xpos = [30, 38]
+    ypos = [38, 46]
+    zpos = [46, 54]
+    occupos = [54, 60]
+    bfacpos = [60, 66]
+    atm_typepos = [77, 79]
+
+    for l in lines:
+        line = l.split()
+        if "ATOM" in line[0] or "HETATM" in line[0]:
+            serial.append(int(l[serialpos[0]:serialpos[1]]))
+            atm_name.append(l[atm_namepos[0]:atm_namepos[1]].strip())
+            val_r_name = l[r_namepos[0]:r_namepos[1]].strip()
+            res_name.append(val_r_name)
+            chain_val = l[chainpos[0]:chainpos[1]].strip()
+            chain.append(chain_val)
+            if chain_val not in var_nchains:
+                var_nchains.append(chain_val)
+            val_r_id = int(l[r_idpos[0]:r_idpos[1]])
+            res_id.append(val_r_id)
+            x = float(l[xpos[0]:xpos[1]])
+            y = float(l[ypos[0]:ypos[1]])
+            z = float(l[zpos[0]:zpos[1]])
+            positions.append([x, y, z])
+            occupancy.append(l[occupos[0]:occupos[1]].strip())
+            temp_factor.append(l[bfacpos[0]:bfacpos[1]].strip())
+            atom_type.append(l[atm_typepos[0]:atm_typepos[1]].strip())
+            ct += 1
+
+    # Create list of atoms
+    tmp_res = res_id[0]
+    resct = 1
+    for i in range(len(chain)):  # pylint: disable=consider-using-enumerate
+        if tmp_res != res_id[i]:
+            tmp_res = res_id[i]
+            resct += 1
+        datb['atoms'].append({
+            "name": atm_name[i],
+            "chain": chain[i],
+            "positions": positions[i],
+            "residue_index": resct,
+            "element": atom_type[i],
+            "residue_name": res_name[i] + str(res_id[i]),
+            "serial": i,
+        })
+
+    # Create list of bonds using the parmed module
+    for i in range(len(top.bonds)):
+        bondpair = top.bonds[i].__dict__
+        atom1 = re.findall(r"\[(\d+)\]", str(bondpair['atom1']))
+        atom2 = re.findall(r"\[(\d+)\]", str(bondpair['atom2']))
+        datb['bonds'].append({
+            'atom2_index': int(atom1[0]),
+            'atom1_index': int(atom2[0])
+        })
+
+    return json.dumps(datb)
 
 
 # # Main dashboard function
